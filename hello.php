02878d4bbeec1592437893e321577b5883bfacfe
harry7potter <55805833+harry7potter@users.noreply.github.com>
2019-10-01 22:13:47 +0530

    Update hello.php

--- hello.php
+++ hello.php
@@ -1,7 +1,7 @@
 <?php
   // Declare the variable 'string' and assign it a value.
   // The <br> is the HTML equivalent to a new line.
-  $string = 'Hello World!<br>';
+  string = 'Hello World!<br>';
 
   // You can echo the variable, similar to the way you would echo a string.
   echo $string;
